27241a9fa835486b1b20412f4e10e1e744c2ef8a
Satish <satish_95raj@outlook.com>
2022-10-18 13:28:58 +0530

    updateCart

--- src/controller/cartController.js
+++ src/controller/cartController.js
@@ -130,37 +130,44 @@ if(!RemoveProduct){
 if(RemoveProduct != 0 && RemoveProduct != 1){
     return res.status(400).send({status : false , message : "Remove product only contain 0 or 1"})
 }
-if(RemoveProduct==1){
 
-    let qty = cart1.items
-        let uptotal = (cart1.totalPrice + (product1.price * Number(quantity))).toFixed(2)
-        for (let i = 0; i < qty.length; i++) {
-            let prod_itemId = qty[i].productId.toString()
-            if (product1._id.toString() == prod_itemId) {
-                let oldQty = qty[i].quantity
-                let newqty = oldQty -  quantity
-                qty[i].quantity = newqty
-                cart1.totalPrice = uptotal
-                await cart1.save();
+let items1 = cart1.items
+for (let i = 0; i < items1.length; i++) {
+    let prod_itemId = items1[i].productId.toString()
+    
+    if (product1._id.toString() == prod_itemId) {
+        let oldQty = items1[i].quantity
+            if(RemoveProduct==1 && oldQty>1){
+           
+            let uptotal = cart1.totalPrice - product1.price 
+            let newqty = oldQty - 1
+            items1[i].quantity = newqty
+            cart1.totalPrice = uptotal
+            await cart1.save();
                 // let existProd = await cartModel.findOneAndUpdate({_id:cartId},checkCart,{new:true})
             
-                return res.status(201).send({ status: true, message: "Exist product update successfully", data: cart1 })
+                return res.status(200).send({ status: true, message: "Exist product update successfully", data: cart1 })
                 }
+                let total = (cart1.totalPrice - (product1.price*cart1.items[i].quantity))
+                cart1.totalPrice=total
+                cart1.items.pop(items1[i])
+                cart1.totalItems= cart1.items.length
+                await cart1.save()
+                return res.status(200).send({ status: true, message: "Exist product delete successfully", data: cart1 })
+            }
+           
+
+            
         }
-        cart1.items.push({ productId: productId, quantity: Number(quantity) })
-        let total = (cart1.totalPrice - (checkProduct.price * Number(quantity))).toFixed(2)
-        cart1.totalPrice = total
-        let count = cart1.totalItems
-        cart1.totalItems = count - 1
-        await cart1.save()
-        return res.status(201).send({ status: true, message: 'Success', data: cart1 })
+        return res.status(404).send({status:false, message:'Product not found or product deleted'})
+       
 
 }
 
 
 
 
-}
+
 // ==================getByUserId==============
 
 const getByUserId= async function (req,res){
